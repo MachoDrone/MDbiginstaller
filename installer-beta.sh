9d667a857c495db827f599437da4af5f872f0c1e
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2025-06-19 13:04:12 -0400

    Update installer-beta.sh

--- installer-beta.sh
+++ installer-beta.sh
@@ -285,4 +285,10 @@ echo -e "- You updated the kernel or systemd (to load new modules)"
 echo -e "- You want Docker group changes to take effect (to use Docker without sudo)"
 echo -e "${GREEN}After reboot, your system will be ready for Nosana node operation.${NC}"
 echo -e "${GREEN}You may run 'sudo apt autoremove' to clean up unused packages.${NC}"
+sudo mkdir /home/md/.nosana
+sudo chown root:root /home/md/.nosana
+sudo chmod 755 /home/md/.nosana
+sudo touch /home/md/.nosana/nosana_key.json
+sudo chown root:root /home/md/.nosana/nosana_key.json
+sudo chmod 644 /home/md/.nosana/nosana_key.json
 sudo systemctl reboot
